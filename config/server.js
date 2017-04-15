ca5cf8b7fc6e25a9cda5b0393b4eed3de2ac6945
GuiPavarina <Guilhermedesaster@gmail.com>
2017-04-15 13:03:31 -0300

    Changed start

--- config/server.js
+++ config/server.js
@@ -38,9 +38,9 @@ app.use(expressSession({
 
 /* efetua o autoload das rotas, dos models e dos controllers para o objeto app */
 consign()
-	.include('app/routes')
-	.then('app/models')
-	.then('app/controllers')
+	.include('./app/routes')
+	.then('./app/models')
+	.then('./app/controllers')
 	.into(app);
 
 
