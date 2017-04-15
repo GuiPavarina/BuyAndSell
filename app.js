541a6bf060ff857a4dc0551737b2f8502f0d48dd
GuiPavarina <Guilhermedesaster@gmail.com>
2017-04-15 13:21:31 -0300

    Changed start

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('./config/server');
 
-const port = 3000
+var port = process.env.PORT || 8080;
 
 app.listen(port, function(){
-	console.log(`Server running on port ${port}`);
+	console.log('Server running');
 })
