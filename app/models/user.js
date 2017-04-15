af18c035a3241c49a362959e26d05228bec64865
GuiPavarina <Guilhermedesaster@gmail.com>
2017-04-15 12:51:39 -0300

    Changed start

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/db_buyandsell');
+mongoose.connect('mongodb://guipavarina:123456@ds021761.mlab.com:21761/heroku_6707t0cv');
+//mongoose.connect('mongodb://localhost/db_buyandsell');
 
 // User Schema
 var UserSchema = mongoose.Schema({
